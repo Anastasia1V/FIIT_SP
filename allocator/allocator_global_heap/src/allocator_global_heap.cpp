d0e6174a5ff8c7efa6c1fa7a77e977571d20e87f
Anastasia Vorovitskaya <VanastasiaR@yandex.ru>
2026-04-15 23:03:35 +0300

    Add allocator_global_heap

--- allocator/allocator_global_heap/src/allocator_global_heap.cpp
+++ allocator/allocator_global_heap/src/allocator_global_heap.cpp
@@ -3,19 +3,27 @@
 
 allocator_global_heap::allocator_global_heap()
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap()", "your code should be here...");
 }
 
 [[nodiscard]] void *allocator_global_heap::do_allocate_sm(
     size_t size)
 {
-    throw not_implemented("[[nodiscard]] void *allocator_global_heap::do_allocate_sm(size_t)", "your code should be here...");
+    if (size == 0) {
+        size = 1;
+    }
+    std::lock_guard<std::mutex> lock(_mutex);
+    void *ptr = ::operator new(size);
+    return ptr;
 }
 
 void allocator_global_heap::do_deallocate_sm(
     void *at)
 {
-    throw not_implemented("void allocator_global_heap::do_deallocate_sm(void *)", "your code should be here...");
+    if (at == nullptr) {
+        return;
+    }
+    std::lock_guard<std::mutex> lock(_mutex);
+    ::operator delete(at);
 }
 
 allocator_global_heap::~allocator_global_heap()
@@ -24,16 +32,16 @@ allocator_global_heap::~allocator_global_heap()
 
 allocator_global_heap::allocator_global_heap(const allocator_global_heap &other)
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap(const allocator_global_heap &other)", "your code should be here...");
 }
 
 allocator_global_heap &allocator_global_heap::operator=(const allocator_global_heap &other)
 {
+    return *this;
 }
 
 bool allocator_global_heap::do_is_equal(const std::pmr::memory_resource &other) const noexcept
 {
-    throw not_implemented("bool allocator_global_heap::do_is_equal(const std::pmr::memory_resource &other) const noexcept", "your code should be here...");
+    return this == &other;
 }
 
 allocator_global_heap::allocator_global_heap(allocator_global_heap &&other) noexcept
@@ -42,4 +50,5 @@ allocator_global_heap::allocator_global_heap(allocator_global_heap &&other) noex
 
 allocator_global_heap &allocator_global_heap::operator=(allocator_global_heap &&other) noexcept
 {
+    return *this;
 }
